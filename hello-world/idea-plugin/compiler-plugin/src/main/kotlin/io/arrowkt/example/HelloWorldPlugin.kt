eca3c138baaa3dfe44e5e7e0b4fab16af10ff178
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-05-12 11:21:55 +0200

    Re-org

---
+++ hello-world/idea-plugin/compiler-plugin/src/main/kotlin/io/arrowkt/example/HelloWorldPlugin.kt
@@ -0,0 +1,23 @@
+package io.arrowkt.example
+
+import arrow.meta.CliPlugin
+import arrow.meta.Meta
+import arrow.meta.invoke
+import arrow.meta.quotes.Transform
+import arrow.meta.quotes.namedFunction
+
+val Meta.helloWorld: CliPlugin
+  get() =
+    "Hello World" {
+      meta(
+        namedFunction({ name == "helloWorld" }) { c ->
+          Transform.replace(
+            replacing = c,
+            newDeclaration =
+            """|fun helloWorld(): Unit =
+               |  println("Hello ΛRROW Meta!")
+               |""".function.syntheticScope
+          )
+        }
+      )
+    }
