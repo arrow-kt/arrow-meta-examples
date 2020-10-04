ececd3394c094c03e98a36554ef993fd5c5621c4
David Stone <david@stabbylambda.com>
2020-10-04 11:56:03 -0700

    Switch from kotlintest to junit
    This example wasn't using anything from kotlintest and the dependency
    was bringing in kotlin-reflect 1.3.50.

--- use-arrow-meta-compiler-plugin/src/test/kotlin/io/arrowkt/example/GivenTest.kt
+++ use-arrow-meta-compiler-plugin/src/test/kotlin/io/arrowkt/example/GivenTest.kt
@@ -1,12 +1,13 @@
 // TODO: It doesn't support a composite package
 package singlepackage
 
-import org.junit.Test
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions;
 
 class GivenTest {
 
     @Test
     fun `coherent polymorphic identity`() {
-        assert(result == "yes!")
+        Assertions.assertTrue(result == "yes!")
     }
 }
